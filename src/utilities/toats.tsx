9c48a0e717d1d086707aa73ef57ad01af19f973a
sumitWebDev <dassumit.ece@gmail.com>
2024-08-15 23:54:54 -0600

    Added Notification Toaster

--- src/utilities/toats.tsx
+++ src/utilities/toats.tsx
@@ -6,11 +6,6 @@ import Toast from 'react-bootstrap/Toast';
   return (
     <Toast onClose={toggleShowB} show={props.isShow} animation={false} className='mobile-only'>
         <Toast.Header>
-        <img
-            src="holder.js/20x20?text=%20"
-            className="rounded me-2"
-            alt=""
-        />
         <strong className="me-auto">Fruit added into Jar</strong>
         </Toast.Header>
         <Toast.Body>Scroll Below to see added fruit!</Toast.Body>
